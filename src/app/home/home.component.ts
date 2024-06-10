2cd70369f8bc2deb98ece9244dd7c39702826660
Eeemlo <emma.lorensson@outlook.com>
2024-06-10 21:19:57 +0200

    Navigering klar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroimageComponent } from '../heroimage/heroimage.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroimageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
