12dfc4cd301db78c45962219184b77de92faade3
Eeemlo <emma.lorensson@outlook.com>
2024-06-10 15:16:08 +0200

    Påbörjat konfiguration

---
+++ src/app/myschedule/myschedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myschedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './myschedule.component.html',
+  styleUrl: './myschedule.component.css'
+})
+export class MyscheduleComponent {
+
+}
