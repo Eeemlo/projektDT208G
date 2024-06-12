565831b984541b0d0b3ab024ef2263c7115da63b
Eeemlo <emma.lorensson@outlook.com>
2024-06-12 10:12:03 +0200

    Ytterligare komponenter till startsida skapade

---
+++ src/app/textonimage/textonimage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textonimage',
+  standalone: true,
+  imports: [],
+  templateUrl: './textonimage.component.html',
+  styleUrl: './textonimage.component.css'
+})
+export class TextonimageComponent {
+
+}
