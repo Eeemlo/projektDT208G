e6d12e84330e69382f5fad2f4fefc15f49596967
Eeemlo <emma.lorensson@outlook.com>
2024-07-14 12:29:34 +0200

    Avinstallerat material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainmenuComponent],
+  imports: [RouterOutlet, MainmenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroimageComponent } from '../heroimage/heroimage.component';
+import { TextonimageComponent } from '../textonimage/textonimage.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroimageComponent],
+  imports: [HeroimageComponent, TextonimageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
