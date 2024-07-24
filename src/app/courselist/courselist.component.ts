a8242f729d5a9c7f274ba6aeb44bb6bba357d5be
Eeemlo <emma.lorensson@outlook.com>
2024-07-24 11:53:26 +0200

    Filtrering av tabell tillagd

--- src/app/courselist/courselist.component.ts
+++ src/app/courselist/courselist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { CourseDataService } from '../services/course-data.service';
 import { Course } from '../models/course';
 import { CommonModule } from '@angular/common';
@@ -7,14 +9,14 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-courselist',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
+  imports: [MatTableModule, CommonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './courselist.component.html',
   styleUrls: ['./courselist.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class CourselistComponent implements OnInit {
-  courses: Course[] = [];
   displayedColumns: string[] = ['courseCode', 'courseName', 'subject', 'points', 'syllabus', 'action'];
+  dataSource: MatTableDataSource<Course> = new MatTableDataSource<Course>();
 
   // Konstruktor för att använda service för kursdata
   constructor(private courseDataService: CourseDataService) {}
@@ -22,7 +24,12 @@ export class CourselistComponent implements OnInit {
   // Prenumerera på data från webbtjänst
   ngOnInit() {
     this.courseDataService.getCourses().subscribe(courses => {
-      this.courses = courses;
+      this.dataSource.data = courses;
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
